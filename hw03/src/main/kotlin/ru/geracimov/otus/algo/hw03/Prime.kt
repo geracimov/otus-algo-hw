b4a8ca0950c0f385f8118bc1a243f4f4b3f94269
geracimov <geracimov@gmail.com>
2023-02-19 23:23:19 +0300

    fix: PrimesBasedPrimeCounter

--- hw03/src/main/kotlin/ru/geracimov/otus/algo/hw03/Prime.kt
+++ hw03/src/main/kotlin/ru/geracimov/otus/algo/hw03/Prime.kt
@@ -34,3 +34,36 @@ class IteratePrimeCounter : PrimeCounter {
     }
 
 }
+
+class PrimesBasedPrimeCounter : PrimeCounter {
+    override fun get(number: Long): Int {
+        var count = 0
+        val size = (number / 2).toInt()
+        val primes = Array(size) { 0L }
+
+        primes[count++] = 2
+
+        for (i in 3..number step 2) {
+            if (isPrime(i, primes)) {
+                primes[count++] = i
+            }
+        }
+        return count
+    }
+
+    private fun isPrime(number: Long, primes: Array<Long>): Boolean {
+        if (number == 2L) return true
+        if (number % 2 == 0L || number == 1L) return false
+
+        var i = 0
+        while (primes[i] <= sqrt(number.toDouble())) {
+            if (number % primes[i] == 0L) {
+                return false
+            }
+            i++
+        }
+
+        return true
+    }
+
+}
